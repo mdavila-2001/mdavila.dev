905b37279879b0752dc8f62b58400cf5f2bbdef3
Marcelo Dávila <dmarcelo201@gmail.com>
2026-03-23 08:44:28 -0400

    feat: Implement an experience section featuring a timeline, internationalization, and updated styling.

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -4,6 +4,7 @@ export const experience = [
         company: "experience.company.opti",
         period: "experience.year_start.opti - experience.year_end.opti",
         side: "right",
+        icon: "smartphone",
         achievements: [
             "experience.achievement1.opti",
             "experience.achievement2.opti"
@@ -19,6 +20,7 @@ export const experience = [
         company: "experience.company.qa",
         period: "experience.year_start.qa - experience.year_end.qa",
         side: "left",
+        icon: "bug_report",
         achievements: [
             "experience.achievement1.qa",
             "experience.achievement2.qa",
@@ -34,6 +36,71 @@ export const experience = [
             "Spring Boot",
             ".NET",
             "Angular"
+        ]
+    },
+    {
+        title: "experience.title.dima",
+        company: "experience.company.dima",
+        period: "experience.year_start.dima - experience.year_end.dima",
+        side: "right",
+        icon: "business_center",
+        achievements: [
+            "experience.achievement1.dima"
+        ],
+        tech: [
+            "AL",
+            "Microsoft Business 365 Dynamics",
+            "Microsoft Report Builder"
+        ]
+    },
+    {
+        title: "experience.title.reporting",
+        company: "experience.company.reporting",
+        period: "experience.year_start.reporting - experience.year_end.reporting",
+        side: "left",
+        icon: "analytics",
+        achievements: [
+            "experience.achievement1.reporting",
+            "experience.achievement2.reporting"
+        ],
+        tech: [
+            "Microsoft SQL Server",
+            "FortiClient",
+            "Tableau"
+        ]
+    },
+    {
+        title: "experience.title.solver",
+        company: "experience.company.solver",
+        period: "experience.year_start.solver - experience.year_end.solver",
+        side: "right",
+        icon: "smartphone",
+        achievements: [
+            "experience.achievement1.solver",
+            "experience.achievement2.solver"
+        ],
+        tech: [
+            "Flutter",
+            "Firebase",
+            "Dart",
+            "Kotlin"
+        ]
+    },
+    {
+        title: "experience.title.upsa",
+        company: "experience.company.upsa",
+        period: "experience.year_start.upsa - experience.year_end.upsa",
+        side: "left",
+        icon: "terminal",
+        achievements: [
+            "experience.achievement1.upsa",
+            "experience.achievement2.upsa"
+        ],
+        tech: [
+            "Laravel",
+            "MySQL",
+            ".NET",
+            "Manual Testing"
         ]
     }
 ];
